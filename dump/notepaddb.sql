4296b6cafdf6364a326cd92f2f3cda3f26fca773
lilitbet <lilitbet007@gmail.com>
2026-02-23 04:06:48 +0300

    Update notepaddb.sql

--- dump/notepaddb.sql
+++ dump/notepaddb.sql
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- База данных: `notepaddb`
 --
+CREATE DATABASE IF NOT EXISTS `notepaddb` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `notepaddb`;
 
 -- --------------------------------------------------------
 
